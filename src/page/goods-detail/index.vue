69b510b58123c8d9a7eb1d4e4fe70c5ef52f8cd5
simon <1138864681@qq.com>
2020-05-08 15:28:05 +0800

    linux测试

--- src/page/goods-detail/index.vue
+++ src/page/goods-detail/index.vue
@@ -37,17 +37,17 @@
       <div class="news">公告：本店推出多件新的单品，均可放心食用。需要发票的请单独备注。</div>
     </section>
 
-    <!-- <tabs /> -->
+    <tabs />
     <chain-slider></chain-slider>
   </div>
 </template>
 
 <script>
-// import Tabs from './Tabs'
+import Tabs from './Tabs'
 import ChainSlider from './ChainSlider'
 export default {
   components: {
-    // Tabs,
+    Tabs,
     ChainSlider
   },
   methods: {
